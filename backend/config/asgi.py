b3e2fbfbdf22aa550a01b1bfa56755957cbb9fe4
henry0295 <103905485+henry0295@users.noreply.github.com>
2026-09-03 12:04:49 -0500

    fix: reordenar importaciones en asgi.py para mejorar la claridad

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -5,8 +5,6 @@ Exposes the ASGI callable as a module-level variable named ``application``.
 
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from core.websocket_auth import JwtAuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
@@ -14,6 +12,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django_asgi_app = get_asgi_application()
 
 # Import routing after Django is initialized
+from channels.routing import ProtocolTypeRouter, URLRouter
+from core.websocket_auth import JwtAuthMiddlewareStack
 from apps.api import routing
 
 application = ProtocolTypeRouter({
